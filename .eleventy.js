7db13162617c884b70eb40ae811bb9f343853415
Roberta Antinolfi <131576108+robertaantinolfi@users.noreply.github.com>
2024-10-11 09:16:42 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
     return {
         dir: {
             input: "src",
+            includes: "_includes",
             output: "public"
         }
     }
